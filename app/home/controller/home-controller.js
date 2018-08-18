a95078f439d10961a715890b011485174034b35a
sarwin-github <dapito.sherwin@yahoo.com>
2018-08-18 20:18:32 +0800

    refactor(home-controller): modified return value for the result

--- app/home/controller/home-controller.js
+++ app/home/controller/home-controller.js
@@ -14,29 +14,28 @@ module.exports.getHome = (req, res) => {
 const timeout = ms => new Promise(res => setTimeout(res, ms));
 
 module.exports.searchBrand = async (req, res) => {
-	
-
 	try {
 		let domain, facebook, twitter;
 
-       	await driver.get('https://www.namecheckr.com/')
+       	await driver.get('https://www.namecheckr.com/');
        	await driver.findElement(By.id('name')).sendKeys(req.query['brand'], Key.RETURN);
-
-       	await timeout(3000)
+       	await timeout(3000);
 
        	domain   = await driver.findElement(By.id('com')).getAttribute("href");
        	facebook = await driver.findElement(By.id('facebook')).getAttribute("href");
        	twitter  = await driver.findElement(By.id('twitter')).getAttribute("href");
 
-        await driver.quit()
+        await driver.quit();
 
         return res.status(200).json({
-        	message  : "Successfully retrieved",
-        	domain   : domain,
-        	facebook : facebook,
-        	twitter  : twitter,
-        	success  : true
-        })
+        	success  : true,
+        	message  : "Data has been successfully retrieved",
+        	result : [
+        		{ domain   : checkIfExist(domain) },
+        		{ facebook : checkIfExist(facebook) },
+        		{ twitter  : checkIfExist(twitter) },
+        	]
+        });
     }
     catch (err) {
     	this.driver.switchTo().alert().then((alert) => alert.dismiss());
@@ -47,4 +46,8 @@ module.exports.searchBrand = async (req, res) => {
     		});
         await driver.quit()
     }
+}
+
+const checkIfExist = data => {
+	return data.split('/')[5] === 'view' ? false : true;
 }
